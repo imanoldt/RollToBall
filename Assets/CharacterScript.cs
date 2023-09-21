77d4ce0d6c102c96baf2aaad08fc7d18a87cca46
xJustCallMeAlex <alexander.kolar1379@gmail.com>
2023-09-21 11:21:26 +0200

    final commit

--- Assets/CharacterScript.cs
+++ Assets/CharacterScript.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEngine.GraphicsBuffer;
 
 public class CharacterScript : MonoBehaviour
 {
     Vector3 newPosition;
     public float speed = 3;
+    public Text text;
     void Start()
     {
         newPosition = transform.position;
